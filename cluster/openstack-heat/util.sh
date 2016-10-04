f495dc5ac6149a4b860b576d67def88547fe1eb9
shashidharatd <shashidhara.td@huawei.com>
2016-10-04 20:16:06 +0530

    Add deploy scripts for skydns dns provider in federation

--- cluster/openstack-heat/util.sh
+++ cluster/openstack-heat/util.sh
@@ -44,6 +44,17 @@ function verify-prereqs() {
  done
 }
 
+# Create a temp dir that'll be deleted at the end of this bash session.
+#
+# Vars set:
+#   KUBE_TEMP
+function ensure-temp-dir() {
+  if [[ -z ${KUBE_TEMP-} ]]; then
+    KUBE_TEMP=$(mktemp -d -t kubernetes.XXXXXX)
+    trap 'rm -rf "${KUBE_TEMP}"' EXIT
+  fi
+}
+
 # Instantiate a kubernetes cluster
 #
 # Assumed vars:
@@ -250,6 +261,8 @@ function configure-kubectl() {
   fi
 
   create-kubeconfig
+
+  create-kubeconfig-for-federation
 }
 
 
